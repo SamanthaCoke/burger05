64b562675cf74e410c86ebedaecb3f295d96b902
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-05-26 10:56:37 -0600

    changed mysql info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "newuser",
-  password: "newuser",
-  database: "burgers_db"
+  user: "xvyk8y98dqaf99ux",
+  password: "xdfu1zy88p35isbh",
+  database: "mlakcx3pj067ut66"
 });
 
 connection.connect(function(err) {
